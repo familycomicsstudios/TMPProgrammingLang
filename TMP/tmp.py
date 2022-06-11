8425550848584e81f9c1631b5f8a80d2e971f61d
familycomicsstudios <106832853+familycomicsstudios@users.noreply.github.com>
2022-06-11 11:15:36 -0500

    Update files

--- TMP/tmp.py
+++ TMP/tmp.py
@@ -1,6 +1,6 @@
 """
 TMP Programming Language
-v0.3.0
+v0.4.0
 Build 06102022
 """
 
@@ -30,29 +30,39 @@ def preprocess(args):
 #Interpret code from text input
 def interpetCode(plainInput):
     global variables
-    stringLiteral = handleControl(plainInput)
-    args = plainInput.split()
-    finalArgs = preprocess(args)
-    fullStringLiteral = " ".join(args)
-    if (finalArgs[0] == "println"):
-        print(fullStringLiteral[8:])
-    if (finalArgs[0] == "print"):
-        print(fullStringLiteral[6:], end='')
-    if (finalArgs[0] == "setvar"):
-        variables[str(finalArgs[1])] = fullStringLiteral[8+len(finalArgs[1]):]
-    if (finalArgs[0] == "input"):
-        variables[str(finalArgs[1])] = input()
-    if (finalArgs[0] == "ifset"):
-        if finalArgs[1] == "True":
-            variables[str(finalArgs[4])] = variables[str(finalArgs[2])]
+    global skipNextLine
+    if skipNextLine < 1:
+        stringLiteral = handleControl(plainInput)
+        args = plainInput.split()
+        finalArgs = preprocess(args)
+        fullStringLiteral = " ".join(args)
+        if len(finalArgs) > 0:
+            if (finalArgs[0] == "println"):
+                print(fullStringLiteral[8:])
+            if (finalArgs[0] == "print"):
+                print(fullStringLiteral[6:], end='')
+            if (finalArgs[0] == "setvar"):
+                variables[str(finalArgs[1])] = fullStringLiteral[8+len(finalArgs[1]):]
+            if (finalArgs[0] == "input"):
+                variables[str(finalArgs[1])] = input()
+            if (finalArgs[0] == "ifset"):
+                if finalArgs[1] == "True":
+                    variables[str(finalArgs[4])] = variables[str(finalArgs[2])]
+                else:
+                    variables[str(finalArgs[4])] = variables[str(finalArgs[3])]
+            if (finalArgs[0] == "eval"):
+                if finalArgs[2] == "=":
+                    if finalArgs[1] == finalArgs[3]:
+                        variables[str(finalArgs[4])] = "True"
+                    else:
+                        variables[str(finalArgs[4])] = "False"
+            if (finalArgs[0] == "if"):
+                if finalArgs[1] == "True":
+                    skipNextLine = 0
+                else:
+                    skipNextLine = int(finalArgs[2])
         else:
-            variables[str(finalArgs[4])] = variables[str(finalArgs[3])]
-    if (finalArgs[0] == "eval"):
-        if finalArgs[2] == "=":
-            if finalArgs[1] == finalArgs[3]:
-                variables[str(finalArgs[4])] = "True"
-            else:
-                variables[str(finalArgs[4])] = "False"
+            skipNextLine -= 1
                 
             
 
@@ -61,6 +71,7 @@ def interpetCode(plainInput):
 
 #Define variables in dictionary
 variables = {"test": "1"}
+skipNextLine = 0
 
 if __name__ == "__main__":
     while True:
